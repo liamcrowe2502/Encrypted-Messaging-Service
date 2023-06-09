f37716c419fd874c35517c72c56e5fe190795b0e
liamcrowe02 <liamcrowe2502@gmail.com>
2023-06-09 19:13:01 +0100

    Now the screen won't change until all fields are filled in with and a prompt appears if they aren't

--- app/src/main/java/com/example/encryptedmessagingservice/Login.java
+++ app/src/main/java/com/example/encryptedmessagingservice/Login.java
@@ -4,23 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class Login extends AppCompatActivity {
+
+    private EditText email;
+    private EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button loginButton = (Button)findViewById(R.id.loginButton);
-        Button switchButton = (Button)findViewById(R.id.switchButton);
+        Button loginButton = findViewById(R.id.loginButton);
+        Button switchButton = findViewById(R.id.switchButton);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.pass);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Login.this, MainPage.class));
+                attemptLogin();
             }
         });
 
@@ -30,6 +40,17 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(Login.this, Register.class));
             }
         });
+    }
 
+    private void attemptLogin() {
+        String emails = email.getText().toString();
+        String password = pass.getText().toString();
+
+        if (TextUtils.isEmpty(emails) || TextUtils.isEmpty(password)) {
+            Snackbar mySnackbar = Snackbar.make(email, "Please fill in all fields", Snackbar.LENGTH_LONG);
+            mySnackbar.show();
+        } else {
+            startActivity(new Intent(Login.this, MainPage.class));
+        }
     }
 }
--- app/src/main/java/com/example/encryptedmessagingservice/Register.java
+++ app/src/main/java/com/example/encryptedmessagingservice/Register.java
@@ -4,23 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class Register extends AppCompatActivity {
+
+    private EditText username;
+    private EditText email;
+    private EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button regButton = (Button)findViewById(R.id.regButton);
-        Button switchButton = (Button)findViewById(R.id.switchButton);
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.pass);
+
+        Button regButton = findViewById(R.id.regButton);
+        Button switchButton = findViewById(R.id.switchButton);
 
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Register.this, MainPage.class));
+                attemptRegistration();
             }
         });
 
@@ -30,5 +42,18 @@ public class Register extends AppCompatActivity {
                 startActivity(new Intent(Register.this, Login.class));
             }
         });
+    }
+
+    private void attemptRegistration() {
+        String usernames = username.getText().toString();
+        String emails = email.getText().toString();
+        String password = pass.getText().toString();
+
+        if (TextUtils.isEmpty(usernames) || TextUtils.isEmpty(emails) || TextUtils.isEmpty(password)) {
+            Snackbar mySnackbar = Snackbar.make(email, "Please fill in all fields", Snackbar.LENGTH_LONG);
+            mySnackbar.show();
+        } else {
+            startActivity(new Intent(Register.this, MainPage.class));
+        }
     }
 }
